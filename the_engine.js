c748b14edeca1782d50ac27d0ea542ef3860ea8f
Mohammad Qasem <Moh.qasim97@gmail.com>
2020-12-16 21:42:00 +0200

    first dynamic page

---
+++ the_engine.js
@@ -0,0 +1,14 @@
+function checkpassward(){
+    var getName = prompt("What is your name?");
+    var getOld = prompt("How old are you? ");
+    var pass = prompt("What is your password?      note:123 is the password");
+  if (pass == 123) {
+  alert("wawoo you are  "+ getOld +"!! by the way welcome to you  "+ getName);
+  } else {
+  alert("Not Correct");
+  }}
+  
+  checkpassward()
+  
+  
+  
