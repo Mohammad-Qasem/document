dad742caa6c76970f056b98872da8f66255bdac6
Mohammad Qasem <Moh.qasim97@gmail.com>
2020-12-17 15:27:13 +0200

    the pasward checker multipple time

---
+++ engine_lab_8.js
@@ -0,0 +1,17 @@
+while (true && (secret !== 123)) {
+    var secret = prompt("Writ the password         NOTE:Passward is 123");
+    if(true){
+        if (secret == 123){
+        alert("the passward is correct welcome to my page")
+        break
+      
+    }else {alert("password is incorrect")
+    }
+
+    }
+    
+
+    }
+  
+    
+  
